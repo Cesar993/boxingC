1411efd0d3236f453b48b5f98c2c97883536f6b5
Cesar993 <ce.serrano@duocuc.cl.com>
2023-10-10 19:11:54 -0300

    BokingC

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+List<object> listaBox = new List<object>();
+
+listaBox.Add(7);
+listaBox.Add(28);
+listaBox.Add(-1);
+listaBox.Add(true);
+listaBox.Add("chair");
+
+
+
+int sum = 0;
+for (int i = 0; i < listaBox.Count; i++)
+{
+    
+    if (listaBox[i] is int)
+    {
+        sum += (int)listaBox[i];
+    }
+    
+};
+    Console.WriteLine($"La suma es de {sum}");
